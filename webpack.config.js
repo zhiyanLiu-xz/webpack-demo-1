ff8cde1cd65e682ef7cccef99c12f0fa515c5b42
zhiyanliu <liuzhiyan138@163.com>
2021-05-13 09:43:44 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -5,6 +6,19 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',
+    filename: 'index.[contenthash].js',
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'My App',
+    template: 'src/assets/index.html'
+  }
+  )],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
