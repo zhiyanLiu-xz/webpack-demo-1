d9c127b3ebff9f0e55eb2514f0415f5066e2185e
zhiyanliu <liuzhiyan138@163.com>
2021-05-13 09:56:49 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,10 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
